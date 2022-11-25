9c8d990ce5869ae67b489cf00db46bc621e6c8f3
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-11-25 18:57:08 +0100

    modificaciones

---
+++ src/components/Dictionary.test.jsx
@@ -0,0 +1,6 @@
+//Hay que verificar que el componente existe
+//Hay que verificar que cuando se pulsa el botón se imprime el resultado en función del resultado de la api
+//Hay que verificar que el resultado de error es correcto cuando corresponda
+//Hay que verificar que el resultado de loading es correcto cuando corresponda
+//Hay que verificar que el resultado de contenido es correcto cuando corresponda
+//Hay que mirar cómo hacer testing del evento onChange en el input
